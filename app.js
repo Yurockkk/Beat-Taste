a8ee5affe80afc233ed8b3da06775f1e9d29d718
Yurockkk <r00631026@ntu.edu.tw>
2017-07-14 16:52:23 -0400

    change to HTML

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ var express = require("express");
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
-app.set("view engine","ejs");
+app.engine('html', require('ejs').renderFile);
+app.set("view engine","html");
 
 
 app.get("/",function(req,res){
-	res.render("circles");
+	res.render("circles.html");
 });
 
 
-app.listen(process.env.PORT, process.env.IP,function(){
+app.listen(process.env.PORT || 3000, process.env.IP,function(){
    console.log("The beat_taste app has started, PORT = " + process.env.PORT); 
 });
